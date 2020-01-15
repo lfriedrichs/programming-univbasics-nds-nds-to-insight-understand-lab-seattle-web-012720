ebe494c9a0633159a4f05baa4ce883d61521f583
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-01-15 16:48:45 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,7 +11,7 @@ end
 def print_first_directors_movie_titles
   index = 0
   while directors_database[0][:movies][index]
-      pp(directors_database[index][:movies][index])
+      pp(directors_database[index][:movies][index][:title])
       index += 1 
   end
       
